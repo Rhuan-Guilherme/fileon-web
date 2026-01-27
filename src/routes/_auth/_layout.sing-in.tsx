775dddc348499a4dddf7ec21409ce968594642d8
Rhuan Guilherme <rhuan23032004@gmail.com>
2026-01-26 23:40:58 -0300

    :lipstick: Alterando estilização das telas de registro e login

--- src/routes/_auth/_layout.sing-in.tsx
+++ src/routes/_auth/_layout.sing-in.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -8,11 +7,18 @@ import z from 'zod';
 import { ErrorForm } from '@/components/ui/error-form';
 import { useMutation } from '@tanstack/react-query';
 import { authenticateUser } from '@/api/authenticate-uset';
-import { Eye, EyeOff, FileText, Lock, Mail } from 'lucide-react';
+import { Eye, EyeOff } from 'lucide-react';
 import { useState } from 'react';
 import { AxiosError } from 'axios';
 import { toast } from 'sonner';
 import { useUserStore } from '@/store/user-store';
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from '@/components/ui/field';
 
 export const Route = createFileRoute('/_auth/_layout/sing-in')({
   component: RouteComponent,
@@ -75,108 +81,103 @@ function RouteComponent() {
   };
 
   return (
-    <div className="w-11/12 max-w-md mx-auto shadow p-6 rounded-lg ">
-      <div className="flex items-center mb-6">
-        <div className="bg-blue-600 p-2 rounded-xl mr-2">
-          <FileText className="text-white" size={20} />
+    <form className="p-6 md:p-8" onSubmit={handleSubmit(handleSingIn)}>
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Bem vindo de volta!</h1>
+          <p className="text-muted-foreground text-balance">
+            Faça o login em sua conta FileOn
+          </p>
         </div>
-        <h1 className="text-foreground font-semibold text-2xl">FileOn</h1>
-      </div>
-
-      <h1 className="text-foreground/90 text-2xl font-bold">
-        Acesse sua conta
-      </h1>
-      <p className="text-muted-foreground text-sm">
-        Organize e solicite documentos de forma simples e segura.
-      </p>
-
-      <form
-        onSubmit={handleSubmit(handleSingIn)}
-        className="flex flex-col gap-4 mt-10"
-      >
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="email" className="text-muted-foreground">
-            Email
-          </Label>
-          <div className="relative">
-            <Mail
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground"
-              size={16}
-            />
-            <Input
-              type="email"
-              id="email"
-              placeholder="exemplo@exemplo.com"
-              {...register('email')}
-              className={`pl-9 placeholder:text-muted-foreground/70 ${errors && errors.email?.message ? ' focus-visible:ring-rose-400' : ''}`}
-            />
-          </div>
-
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input
+            {...register('email')}
+            id="email"
+            placeholder="email@example.com"
+            type="email"
+            required
+          />
           {errors && errors.email?.message && (
             <ErrorForm message={errors.email.message} />
           )}
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="password" className="text-muted-foreground">
-            Senha
-          </Label>
-          <div className="relative">
-            <Lock
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground"
-              size={16}
-            />
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              {...register('password')}
-              placeholder="••••••••"
-              className={`pl-9  placeholder:text-muted-foreground/70 ${errors && errors.password?.message ? ' focus-visible:ring-rose-400' : ''}`}
-            />
-            {showPassword ? (
-              <Eye
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-muted-foreground cursor-pointer"
-                size={16}
-              />
-            ) : (
-              <EyeOff
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-muted-foreground cursor-pointer"
-                size={16}
-              />
-            )}
+        </Field>
+        <Field className="relative">
+          <div className="flex items-center">
+            <FieldLabel htmlFor="password">Senha</FieldLabel>
+            <a
+              href="#"
+              className="ml-auto text-sm underline-offset-2 hover:underline"
+            >
+              Esqueceu sua senha?
+            </a>
           </div>
-
+          <Input
+            {...register('password')}
+            id="password"
+            type={showPassword ? 'text' : 'password'}
+            placeholder="••••••••"
+            required
+          />
           {errors && errors.password?.message && (
             <ErrorForm message={errors.password.message} />
           )}
-        </div>
+          <div>
+            <button
+              type="button"
+              className="absolute top-1/2 right-2 w-5 h-5 cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <Eye className="w-4 h-4 " />
+              ) : (
+                <EyeOff className="w-4 h-4 " />
+              )}
+            </button>
+          </div>
+        </Field>
 
-        <div>
-          <Button type="submit" disabled={isPending} className="w-full">
-            {isPending ? 'Entrando...' : 'Entrar na plataforma'}
+        <Field>
+          <Button type="submit" disabled={isPending}>
+            {isPending ? 'Entrando...' : 'Acessar a plataforma'}
           </Button>
-        </div>
-
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-zinc-200"></div>
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-3 bg-white text-zinc-400">ou</span>
-          </div>
-        </div>
-
-        <p className="text-muted-foreground text-sm flex justify-center">
-          Ainda não tem acesso?
-          <Link
-            to="/sing-up"
-            className="ml-1 text-blue-500 font-semibold hover:underline"
-          >
-            Criar uma conta gratuita
-          </Link>
-        </p>
-      </form>
-    </div>
+        </Field>
+        <FieldSeparator className="*:data-[slot=field-separator-content]:bg-card">
+          OU
+        </FieldSeparator>
+        <Field className="grid grid-cols-3 gap-4">
+          <Button variant="outline" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
+                fill="currentColor"
+              />
+            </svg>
+            <span className="sr-only">Login with Apple</span>
+          </Button>
+          <Button variant="outline" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                fill="currentColor"
+              />
+            </svg>
+            <span className="sr-only">Login with Google</span>
+          </Button>
+          <Button variant="outline" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <path
+                d="M6.915 4.03c-1.968 0-3.683 1.28-4.871 3.113C.704 9.208 0 11.883 0 14.449c0 .706.07 1.369.21 1.973a6.624 6.624 0 0 0 .265.86 5.297 5.297 0 0 0 .371.761c.696 1.159 1.818 1.927 3.593 1.927 1.497 0 2.633-.671 3.965-2.444.76-1.012 1.144-1.626 2.663-4.32l.756-1.339.186-.325c.061.1.121.196.183.3l2.152 3.595c.724 1.21 1.665 2.556 2.47 3.314 1.046.987 1.992 1.22 3.06 1.22 1.075 0 1.876-.355 2.455-.843a3.743 3.743 0 0 0 .81-.973c.542-.939.861-2.127.861-3.745 0-2.72-.681-5.357-2.084-7.45-1.282-1.912-2.957-2.93-4.716-2.93-1.047 0-2.088.467-3.053 1.308-.652.57-1.257 1.29-1.82 2.05-.69-.875-1.335-1.547-1.958-2.056-1.182-.966-2.315-1.303-3.454-1.303zm10.16 2.053c1.147 0 2.188.758 2.992 1.999 1.132 1.748 1.647 4.195 1.647 6.4 0 1.548-.368 2.9-1.839 2.9-.58 0-1.027-.23-1.664-1.004-.496-.601-1.343-1.878-2.832-4.358l-.617-1.028a44.908 44.908 0 0 0-1.255-1.98c.07-.109.141-.224.211-.327 1.12-1.667 2.118-2.602 3.358-2.602zm-10.201.553c1.265 0 2.058.791 2.675 1.446.307.327.737.871 1.234 1.579l-1.02 1.566c-.757 1.163-1.882 3.017-2.837 4.338-1.191 1.649-1.81 1.817-2.486 1.817-.524 0-1.038-.237-1.383-.794-.263-.426-.464-1.13-.464-2.046 0-2.221.63-4.535 1.66-6.088.454-.687.964-1.226 1.533-1.533a2.264 2.264 0 0 1 1.088-.285z"
+                fill="currentColor"
+              />
+            </svg>
+            <span className="sr-only">Login with Meta</span>
+          </Button>
+        </Field>
+        <FieldDescription className="text-center">
+          Não tem uma conta? <Link to="/sing-up">Cadastre-se</Link>
+        </FieldDescription>
+      </FieldGroup>
+    </form>
   );
 }
--- src/routes/_auth/_layout.tsx
+++ src/routes/_auth/_layout.tsx
@@ -1,11 +1,15 @@
 import { useUserStore } from '@/store/user-store';
 import { createFileRoute, Navigate, Outlet } from '@tanstack/react-router';
 
+import { cn } from '@/lib/utils';
+import { Card, CardContent } from '@/components/ui/card';
+import { FieldDescription } from '@/components/ui/field';
+
 export const Route = createFileRoute('/_auth/_layout')({
   component: RouteComponent,
 });
 
-function RouteComponent() {
+function RouteComponent({ className, ...props }: React.ComponentProps<'div'>) {
   const { user } = useUserStore();
 
   if (user) {
@@ -13,11 +17,33 @@ function RouteComponent() {
   }
 
   return (
-    <div className="grid items-center h-screen p-3">
-      <div className="hidden w-full h-full rounded-xl"></div>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <div className={cn('flex flex-col gap-6', className)} {...props}>
+          <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              <Outlet />
+              <div className="bg-muted relative hidden md:block">
+                <img
+                  src="/scream-light.png"
+                  alt="Image"
+                  className="absolute inset-0 h-[110%] w-full object-cover block dark:hidden"
+                />
 
-      <div className="flex items-center justify-center">
-        <Outlet />
+                <img
+                  src="/scream.png"
+                  alt="Image"
+                  className="absolute inset-0 h-full w-full object-cover hidden dark:block dark:brightness-[0.9] dark:grayscale"
+                />
+              </div>
+            </CardContent>
+          </Card>
+          <FieldDescription className="px-6 text-center">
+            Ao clicar em continuar, você concorda com nossos termos.
+            <a href="#">Termos de serviços</a> e{' '}
+            <a href="#">Política de Privacidade</a>.
+          </FieldDescription>
+        </div>
       </div>
     </div>
   );
