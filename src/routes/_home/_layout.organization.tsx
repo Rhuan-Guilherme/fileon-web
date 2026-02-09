b3c7d6b6d285bad44a05c45148f9306d4b7f3124
Rhuan Guilherme <rhuan23032004@gmail.com>
2026-02-09 19:49:55 -0300

    Refactor organization management layout to use cards and improve UI components

--- src/routes/_home/_layout.organization.tsx
+++ src/routes/_home/_layout.organization.tsx
@@ -3,14 +3,10 @@ import { Button } from '@/components/ui/button';
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table';
+import { Card } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { ArrowRight, Crown, UserLock, Users } from 'lucide-react';
+import { useUserStore } from '@/store/user-store';
 
 interface Organization {
   id: string;
@@ -18,6 +14,14 @@ interface Organization {
   status: string;
   createdAt: string;
   userId: string;
+  organizationMembers: {
+    role: string;
+  }[];
+  user: {
+    name: string;
+    email: string;
+  };
+  _count: { organizationMembers: number };
 }
 
 export const Route = createFileRoute('/_home/_layout/organization')({
@@ -28,6 +32,8 @@ export const Route = createFileRoute('/_home/_layout/organization')({
 });
 
 function RouteComponent() {
+  const { user } = useUserStore();
+
   const { data: organizations } = useQuery({
     queryKey: ['organizationsUser'],
     queryFn: getOrganizationsUser,
@@ -35,61 +41,70 @@ function RouteComponent() {
   });
 
   return (
-    <div className="bg-muted/50 rounded-xl p-5 ">
+    <div className=" rounded-xl p-5 ">
       <Button variant="outline">Criar Organização</Button>
-      <Table className="mt-10 overflow-hidden rounded-2xl border">
-        <TableHeader>
-          <TableRow className="bg-muted/60 hover:bg-muted">
-            <TableHead className="w-60 p-4">Organização</TableHead>
-            <TableHead className="p-4">Status</TableHead>
-            <TableHead className="p-4">Criada em</TableHead>
-            <TableHead className="text-right p-4 w-20">Ação</TableHead>
-          </TableRow>
-        </TableHeader>
 
-        <TableBody>
-          {organizations?.data.map((org: Organization) => (
-            <TableRow
-              key={org.id}
-              className="transition-colors hover:bg-muted/40"
-            >
-              {/* Organização */}
-              <TableCell className="font-medium flex items-center gap-3 p-4">
-                <div className="flex h-9 w-9 items-center justify-center rounded-lg bg-primary/10 text-primary text-sm font-semibold">
-                  {org.name.charAt(0).toUpperCase()}
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {organizations?.data.map((org: Organization) => (
+          <Card className="p-5">
+            <div className="flex justify-center w-full">
+              <div className="flex gap-2.5 w-full">
+                <div className="w-12 h-12 bg-accent rounded-md text-center leading-12 font-semibold text-2xl">
+                  {org.name.charAt(0)}
                 </div>
-                <span className="truncate">{org.name}</span>
-              </TableCell>
-
-              {/* Status */}
-              <TableCell>
-                <span
-                  className={`
-              inline-flex items-center rounded-full px-3 py-1 text-xs font-medium
-              ${
-                org.status === 'ACTIVE'
-                  ? 'bg-emerald-500/10 text-emerald-600'
-                  : 'bg-zinc-500/10 text-zinc-500'
-              }
-            `}
-                >
-                  {org.status === 'ACTIVE' ? 'Ativa' : 'Inativa'}
+                <div className="space-y-1">
+                  <p className="text-foreground font-semibold">{org.name}</p>
+                  <p className="text-xs text-muted-foreground">
+                    Criada em{' '}
+                    {new Date(org.createdAt).toLocaleDateString('pt-BR')}
+                  </p>
+                </div>
+              </div>
+              <div className="w-full flex-1">
+                <span className="bg-emerald-500/10 text-emerald-600 py-1.5 px-4 rounded-full font-semibold text-xs">
+                  {org.status}
                 </span>
-              </TableCell>
+              </div>
+            </div>
 
-              {/* Data */}
-              <TableCell className="text-muted-foreground">
-                {new Date(org.createdAt).toLocaleDateString('pt-BR')}
-              </TableCell>
+            <Separator />
 
-              {/* Plano / Valor */}
-              <TableCell className="text-right font-medium p-4">
-                <Button variant="secondary">Editar</Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+            <div className="space-y-3 text-sm">
+              <div className="flex justify-between">
+                <div className="flex items-center justify-center gap-2">
+                  <Crown className="w-4 h-4" />
+                  <p>Proprietário</p>
+                </div>
+                <p>{org.user.email === user?.email ? 'Você' : org.user.name}</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex justify-between">
+                <div className="flex items-center justify-center gap-2">
+                  <Users className="w-4 h-4" />
+                  <p>Membros</p>
+                </div>
+                <p>{org._count.organizationMembers}</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex justify-between">
+                <div className="flex items-center justify-center gap-2">
+                  <UserLock className="w-4 h-4" />
+                  <p>Seu cargo</p>
+                </div>
+                <p>{org.organizationMembers[0]?.role}</p>
+              </div>
+            </div>
+
+            <Button>
+              <ArrowRight /> Acessar
+            </Button>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
